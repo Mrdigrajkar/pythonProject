481e028162ab46c90be879265221cdc5aba886aa
kdigrajkar <kunaldigrajkar2@gmail.com>
2024-04-18 02:11:28 +0530

    Day1_Practice

---
+++ Day1/DataTypes.py
@@ -0,0 +1,40 @@
+# #What is Data Type
+#
+# #DataType
+# A collection of a group of data value, It contains variable.
+# It is a Dynamically Typed programming language.
+# What is variable.
+# Variable means is a container which stores data.
+# Memory allocated when the value are stored in variable
+# Every variable must have some Data Type , But it is not mandatory to specify the datatype in front of variable.
+
+a=100;
+b=150;
+print(a+b)
+
+# a=100
+# b=150
+#
+# here a and b are dataType and 100 and 150 are variable.
+
+########################## IMPORTANT ##########################
+
+# Type of DataType in python
+
+# Text Type : str
+# Numeric Type : int , float -- for decimal
+# Sequence Type : list , tuple
+# Mapping Type : dict
+# Set Type : set
+# Boolean type : bool -- (True/False)
+
+
+
+
+
+
+
+
+
+
+
