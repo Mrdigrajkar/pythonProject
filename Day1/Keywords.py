22760c421af5409d54290b31bada96b2ff334810
kdigrajkar <kunaldigrajkar2@gmail.com>
2024-04-18 01:32:44 +0530

    Day1_Practice

---
+++ Day1/Keywords.py
@@ -0,0 +1,13 @@
+# --------------------------- Keywords ------------------------------------------------------
+#
+# Keywords are reserved words in python.
+# to display all keyword
+
+import keyword
+print(keyword.kwlist)
+
+#output
+#['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break',
+# 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally',
+# 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal',
+# 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
---
+++ Day1/MyfirstProg.py
@@ -0,0 +1,28 @@
+#--------------------------- First Program ------------------------------------------------------
+
+print("Hello Buddy iam , very much excited to start my programming!!!!!")
+
+#--------------------------- Comments ------------------------------------------------------
+
+# indicates single line comment
+
+#for multiple line comment
+#There are 3 options -
+# 1. can use """ at start    """ at end
+# 2. can use ''' at start   ''' at end
+# 3. can use ctrl + / (Both to apply comment and to remove also)
+# This is python program
+# This is python program
+# This is python program
+# This is python program
+
+
+
+
+
+
+
+
+
+
+
